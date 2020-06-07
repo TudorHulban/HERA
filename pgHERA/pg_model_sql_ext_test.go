2d9a1421f2b3dc88258bb8960b8e2e4d2cd0178a
Tudor Hulban <tudi@J3710>
2020-06-07 19:00:04 +0300

    tests with interfaces now

--- pgHERA/pg_model_sql_ext_test.go
+++ pgHERA/pg_model_sql_ext_test.go
@@ -2,7 +2,9 @@ package pghera_test
 
 import (
 	"net/http/httptest"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/TudorHulban/HERA/pgHERA"
 	"github.com/gofiber/fiber"
@@ -16,6 +18,7 @@ type TRoute struct {
 	Route string
 }
 
+// test hera using satisifed interfaces.
 func TestWFiber(t *testing.T) {
 	h, errCo := pghera.New(pghera.LocalDBInfo, 3, true)
 	if assert.Nil(t, errCo) {
@@ -24,11 +27,14 @@ func TestWFiber(t *testing.T) {
 		// create table. ignore error, we need the table.
 		h.CreateTable(interface{}(&TRoute{}), false)
 
+		var iDDL pghera.IHSQL
+		iDDL = h
+
 		app := fiber.New()
 		app.Get("/:param", func(c *fiber.Ctx) {
 			p := c.Params("param")
 			h.L.Debug("p: ", p)
-			c.Send(h.InsertModel(&TRoute{
+			c.Send(iDDL.InsertModel(&TRoute{
 				Route: p,
 			}))
 		})
@@ -36,9 +42,8 @@ func TestWFiber(t *testing.T) {
 		h.L.Debug("starting fiber")
 		go app.Listen(3000)
 
-		resp, err := app.Test(httptest.NewRequest("GET", "/:param", nil))
+		resp, err := app.Test(httptest.NewRequest("GET", "/"+strconv.FormatInt(time.Now().Unix(), 10), nil))
 		utils.AssertEqual(t, nil, err, "app.Test(req)")
 		utils.AssertEqual(t, 200, resp.StatusCode, "Status code")
-
 	}
 }
